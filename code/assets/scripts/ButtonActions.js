b38f9db4a5f4f22cf2f21b0f8b6cbcf9f6711e73
ccoreil14 <ccoreil14@gmail.com>
2016-10-10 14:46:18 -0700

    updated squareWorkshop.html so that it has everything like cubeWorkshop.html and added a squareTHREE.js file for it as well. Renamed some ButtonActions.js functions so it will be individualized for each page

--- code/assets/scripts/ButtonActions.js
+++ code/assets/scripts/ButtonActions.js
@@ -2,7 +2,7 @@ var arrowIsVisible = true;
 
 
 
-function toggleDynSize(){
+function toggleDynSizePrism(){
     if(arrowIsVisible === false){
         document.getElementById("toggleBtn").innerText = "Arrows Off";
         for(i = 0; i <3; i++){
@@ -20,6 +20,27 @@ function toggleDynSize(){
         lineX.visible = false;
         lineY.visible = false;
         lineZ.visible = false;
+        arrowIsVisible = false;    
+    }
+}
+
+
+function toggleDynSizeRect(){
+    if(arrowIsVisible === false){
+        document.getElementById("toggleBtn").innerText = "Arrows Off";
+        for(i = 0; i <2; i++){
+            arrows[i].visible = true;
+        }
+        lineX.visible = true;
+        lineY.visible = true;
+        arrowIsVisible = true;
+    }else{
+        document.getElementById("toggleBtn").innerText = "Arrows On";
+        for(i = 0; i <2; i++){
+            arrows[i].visible = false;
+        }
+        lineX.visible = false;
+        lineY.visible = false;
         arrowIsVisible = false;    
     }
 }
--- code/assets/scripts/math.js
+++ code/assets/scripts/math.js
@@ -1,9 +1,9 @@
-function rectArea() {
-
+function rectArea(x,y) {
+    return x*y;
 }
 
-function perimeter() {
-
+function perimeter(x,y) {
+    return (x*2)+(y*2);
 }
 
 function circArea() {
